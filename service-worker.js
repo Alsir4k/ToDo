23b69b3963779baaaa624b4d4aff8effa7a496b1
Alsir4k <121882736+Alsir4k@users.noreply.github.com>
2025-03-23 18:03:05 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+// const CACHE_NAME = "cache-v1";
+// const urlsToCache = ["/", "/index.html", "/styles.css", "/app.js"]; // Add more URLs to cache
+// const self = this;
+
+// // filepath: public/service-worker.js
+// self.addEventListener("install", (event) => {
+//   console.log("Service Worker installed.");
+// });
+
+// self.addEventListener("activate", (event) => {
+//   console.log("Service Worker activated.");
+// });
+
+// self.addEventListener("fetch", (event) => {
+//   console.log("Fetching:", event.request.url);
+// });
+
+console.log("Service Worker installed.");
